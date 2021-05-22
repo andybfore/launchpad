9825df175e4b8e9814058a1c943d2401e6a93d36
andybfore <84645169+andybfore@users.noreply.github.com>
2021-05-22 23:48:54 +0200

    initial commit

---
+++ Launchpad.js
@@ -0,0 +1,53 @@
+class Launchpad {
+
+    grid;
+    soundsFolder = 'sounds';
+
+    constructor(launchset) {
+        this.launchset = launchset;
+        this.createGrid();
+        this.createButtons();
+        this.pressButtons();
+        document.getElementById('launchpad').append(this.grid);
+    }
+
+    createGrid() {
+        this.grid = document.createElement('div');
+        this.grid.classList.add('grid');
+    }
+
+    createButtons() {
+        this.launchset.forEach((set, i) => {
+            let button = document.createElement('button');
+            this.grid.append(button);
+
+            let audio = new Audio(this.soundsFolder + '/' + set.name + '.wav');
+
+            button.addEventListener('click', () => {
+                audio.pause();
+                audio.currentTime = 0;
+                audio.play();
+                button.classList.add('active');
+                setTimeout(() => {
+                    button.classList.remove('active');
+                }, 100);
+            });
+
+            this.launchset[i].button = button;
+
+        })
+    }
+
+    pressButtons() {
+        document.addEventListener('keydown', (e) => {
+            this.launchset.forEach(set => {
+                if (e.key === set.key) {
+                    set.button.click();
+                }
+            })
+        })
+    }
+
+}
+
+let launchpad = new Launchpad(launchset);
---
+++ launchset.js
@@ -0,0 +1,38 @@
+const launchset = [
+    {
+        name: 'hi-hats-1',
+        key: 'q'
+    },
+    {
+        name: 'hi-hats-2',
+        key: 'w'
+    },
+    {
+        name: 'hat-1',
+        key: 'e'
+    },
+    {
+        name: 'snare-1',
+        key: 'a'
+    },
+    {
+        name: 'snare-2',
+        key: 's'
+    },
+    {
+        name: 'clap',
+        key: 'd'
+    },
+    {
+        name: 'kick-1',
+        key: '<'
+    },
+    {
+        name: 'kick-2',
+        key: 'z'
+    },
+    {
+        name: 'kick-3',
+        key: 'x'
+    }
+]
